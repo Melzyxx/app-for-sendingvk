e7a05e499746eeaf2f1d70aa8a51fa23b6fa440d
melzyxx <farmbsbraw@gmail.com>
2021-10-18 20:10:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Kivy application for sending messages VK
+
+Rus: Приложение для рассылки сообщений среди подписчиков группы в ВКонтакте
+
+##Using
+
+Eng:
+10 Line - Paste your token vk group
+11 Line - Patse your id vk group
+
+Rus:
+10 Строка - Вставьте свой токен группы вк
+11 Строка - Вставьте ваш айди группы вк
+
---
+++ app.py
@@ -0,0 +1,54 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+from saya import Vk
+import json
+
+#settings
+VK_TOKEN = "вк токен"
+VK_ID = 1
+
+vk = Vk(token = VK_TOKEN)
+del VK_TOKEN
+
+def get_users(group_id:int = 1) -> list:
+	response = vk.groups.getMembers(
+		group_id = group_id,
+		filter = "time_asc"
+	)
+	return response["response"]["items"]
+
+class SenderApp(App):
+	
+	def __init__(self):
+		super().__init__()
+		self.input = TextInput(text = "text", padding = (40, 40))
+	
+	def click(self, instance):
+		text = self.input.text
+		self.input.text = ""
+		users_list = get_users(VK_ID)
+		for user_id in users_list:
+			try:
+				vk.messages.send(
+					user_id = user_id,
+					message = text,
+					random_id = 0
+				)
+			except:
+				pass #No permission
+	
+	def build(self):
+		root = BoxLayout(orientation = "vertical")
+		
+		root.add_widget(self.input)
+		
+		button = Button(text = "SEND ALL", on_press = self.click)
+		root.add_widget(button)
+		
+		return root
+		
+if __name__ == "__main__":
+	SenderApp().run()
